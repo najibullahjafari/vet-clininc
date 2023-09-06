9608a8e60ce64eaf13dc8c164aa4f5fb6aa63222
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-09-06 07:10:30 +0430

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,29 +5,30 @@ CREATE INDEX ON patients (id);
 CREATE TABLE medical_histories(ID INT, admitted_at TIMESTAMP, patient_id INT, status VARCHAR(90),
 PRIMARY KEY(ID),
 FOREIGN KEY(patient_id) REFERENCES patients(id));
-CREATE INDEX ON medical_histories (ID);
+CREATE INDEX ON medical_histories (patient_id);
 
 CREATE TABLE treatments(id INT PRIMARY KEY,  type VARCHAR(60), name VARCHAR(50));
 CREATE INDEX ON treatments (id);
 
-
-
 -- Now a talbe to join the id of treatment and medical history
-CREATE TABLE medical_treatment (
+CREATE TABLE medical_histories_has_treatments (
     medical_history_id INT,
     treatment_id INT,
     FOREIGN KEY (medical_history_id) REFERENCES medical_histories(ID),
     FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 );
+CREATE INDEX ON medical_histories_has_treatments (medical_history_id);
+CREATE INDEX ON medical_histories_has_treatments (treatment_id);
+
 
 CREATE TABLE invoices(id SERIAL PRIMARY KEY, total_amount DECIMAL, generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INT,
 FOREIGN KEY(medical_history_id) REFERENCES medical_histories(ID));
-CREATE INDEX ON invoices (id);
-
+CREATE INDEX ON invoices (medical_history_id);
 
 CREATE TABLE invoice_item(id SERIAL PRIMARY KEY, unit_price DECIMAL, quantity INT, total_price DECIMAL, invoice_id INT, treatment_id INT,
 FOREIGN KEY(invoice_id) REFERENCES invoices(id),
 FOREIGN KEY(treatment_id) REFERENCES treatments(id));
-CREATE INDEX ON invoice_item (id);
+CREATE INDEX ON invoice_item (invoice_id);
+CREATE INDEX ON invoice_item (treatment_id);
 
 
